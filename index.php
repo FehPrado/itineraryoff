091683d192f5f794f44e0a7683f11586a5f28dd4
FehPrado <pradofelipe20032002@gmail.com>
2023-01-16 22:46:38 -0300

    new commit

---
+++ index.php
@@ -0,0 +1,7 @@
+<html>
+  <head>
+    <meta http-equiv="Refresh" content="1; url=/Web" />
+  </head>
+  <body>
+  </body>
+</html>
---
+++ s
@@ -0,0 +1,5 @@
+<?php
+
+exec('google-chrome http://localhost:8000/');
+exec('php -S localhost:8000');
+
